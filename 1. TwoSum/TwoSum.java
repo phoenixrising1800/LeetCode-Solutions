90c7eaf91c563cb4654100979afb8cd5234ab4b5
Nixy Camacho <phoenixrising1800@gmail.com>
2022-12-12 18:38:58 -0500

    Finish exercise 1

--- 1. TwoSum/TwoSum.java
+++ 1. TwoSum/TwoSum.java
@@ -7,7 +7,17 @@
 // @lc code=start
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        
+        int[] ind = new int[2];
+        for (int i=0; i<nums.length; i++) {
+            int current = nums[i];
+            for (int j=i+1; j<nums.length; j++) {
+                if (nums[j] == (target-current)) {
+                    ind[0] = i;
+                    ind[1] = j;
+                }
+            }
+        }
+        return ind;
     }
 }
 // @lc code=end
--- 1. TwoSum/two_sum.py
+++ 1. TwoSum/two_sum.py
@@ -7,6 +7,14 @@
 # @lc code=start
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        ind = [0] * 2
+        for i in range(len(nums)):
+            temp = nums[i]
+            for j in range(i+1, len(nums)):
+                if nums[j] == (target - temp):
+                    ind[0] = i
+                    ind[1] = j
+        return ind
         
 # @lc code=end
 
